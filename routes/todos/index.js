738b0338286f7ae1c05c56bf52c1150d2ecb551c
dannasetia <dannasetia@gmail.com>
2019-12-06 00:49:35 +0700

    two commit

--- routes/todos/index.js
+++ routes/todos/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-router.get("/:email", require("./controller").getAll);
+router.get("/todos/:email", require("./controller").getAll);
 router.post("/", require("./controller").addOne);
 router.put("/:id", require("./controller").updateOne);
 router.delete("/:id", require("./controller").deleteOne);
